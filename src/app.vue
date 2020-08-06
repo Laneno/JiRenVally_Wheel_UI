eae9fe00df5a6d3cde44182fb39f04041718557f
lann <lan-wu@hotmail.com>
2020-08-06 20:38:54 +0800

    init

---
+++ src/app.vue
@@ -0,0 +1,16 @@
+<body>
+<div id="app">
+    {{message}}
+</div>
+
+<script src="./node_modules/vue/dist/vue.min.js"></script>
+<script src="."></script>
+<script>
+    new Vue({
+        el:'#app',
+        data:{
+            message: 'B-UI'
+        }
+    })
+</script>
+</body>
